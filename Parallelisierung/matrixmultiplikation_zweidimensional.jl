6e21f1bcb9c26b399c7907bac89ae40a1c34184c
Isabel Heisters <i.heisters@fz-juelich.de>
2017-03-27 08:35:13 +0200

    Fehler in matrixmult parallel behoben

--- Parallelisierung/matrixmultiplikation_zweidimensional.jl
+++ Parallelisierung/matrixmultiplikation_zweidimensional.jl
@@ -29,14 +29,14 @@ function matrixmultiplikation(A::Array{Int64,1},u::Array{Float64,2})
 end
 
 #Eine geraenderte Matrix wird uebergeben. Von den nebenliegenden Prozessoren werden die raender gefuellt
-function halo_austausch!(id::Int64, size::Int64, x::Array{Float64,2})
+function halo_austausch!(id::Int64, num_procs::Int64, x::Array{Float64,2}, anzx::Int64, anzy::Int64)
     #bei einem prozessor garnichts machen
-    if size!=1
+    if num_procs!=1
         
         #alle sachen die ausgetauscht werden muessen
         oben=x[2:2,2:anzy+1]
         unten=x[anzx+1:anzx+1, 2:anzy+1]
-        links=x[2:anzx+1]
+        links=x[2:anzx+1, 2:2]
         rechts=x[2:anzx+1, anzy+1:anzy+1]
         obenbek=zeros(oben)
         untenbek=zeros(unten)
@@ -44,14 +44,14 @@ function halo_austausch!(id::Int64, size::Int64, x::Array{Float64,2})
         rechtsbek=zeros(rechts)
         
         #Als erstes wird spaltenweise getauscht nach rechts und nach links, zuerst senden alle geraden spalten, dann #die ungeraden
-        if id%sqrt(size)%2==0
+        if id%sqrt(num_procs)%2==0
             #in der ersten spalte wird nur nach rechts getauscht
-            if id%sqrt(size)==0 
+            if id%sqrt(num_procs)==0 
                 MPI.Send(rechts,id+1,0,MPI.COMM_WORLD)
                 MPI.Recv!(rechtsbek,id+1,0,MPI.COMM_WORLD)
                 x[2:anzx+1,anzy+2:anzy+2]=rechtsbek
             #in der letzten spalte wird nur nach links getauscht
-            elseif id%sqrt(size)==sqrt(size)-1
+            elseif id%sqrt(num_procs)==sqrt(num_procs)-1
                 MPI.Send(links,id-1,0,MPI.COMM_WORLD)
                 MPI.Recv!(linksbek,id-1,0,MPI.COMM_WORLD)
                 x[2:anzx+1,1:1]=linksbek
@@ -65,7 +65,7 @@ function halo_austausch!(id::Int64, size::Int64, x::Array{Float64,2})
             end
         else
             #in der letzten spalte wird nur nach links getauscht
-            if id%sqrt(size)==sqrt(size)-1
+            if id%sqrt(num_procs)==sqrt(num_procs)-1
                 MPI.Recv!(linksbek,id-1,0,MPI.COMM_WORLD)
                 x[2:anzx+1,1:1]=linksbek
                 MPI.Send(links,id-1,0,MPI.COMM_WORLD)
@@ -80,38 +80,38 @@ function halo_austausch!(id::Int64, size::Int64, x::Array{Float64,2})
         end
         
         #jetzt wird spaltenweise getauscht, erst die geraden spalten senden dann die ungeraden
-        if floor(id/sqrt(size))%2==0
+        if floor(id/sqrt(num_procs))%2==0
             #in der ersten zeile wird nur nach unten getauscht
-            if floor(id/sqrt(size))==0 
-                MPI.Send(unten,convert(Int32,id+sqrt(size)),0,MPI.COMM_WORLD)
-                MPI.Recv!(untenbek,convert(Int32,id+sqrt(size)),0,MPI.COMM_WORLD)
+            if floor(id/sqrt(num_procs))==0 
+                MPI.Send(unten,convert(Int64,id+sqrt(num_procs)),0,MPI.COMM_WORLD)
+                MPI.Recv!(untenbek,convert(Int64,id+sqrt(num_procs)),0,MPI.COMM_WORLD)
                 x[anzx+2:anzx+2,2:anzy+1]=untenbek
             #in der letzten zeile wird nur nach oben getauscht
-            elseif floor(id/sqrt(size))==sqrt(size)-1
-                MPI.Send(oben,convert(Int32,id-sqrt(size)),0,MPI.COMM_WORLD)
-                MPI.Recv!(obenbek,convert(Int32,id-sqrt(size)),0,MPI.COMM_WORLD)
+            elseif floor(id/sqrt(num_procs))==sqrt(num_procs)-1
+                MPI.Send(oben,convert(Int64,id-sqrt(num_procs)),0,MPI.COMM_WORLD)
+                MPI.Recv!(obenbek,convert(Int64,id-sqrt(num_procs)),0,MPI.COMM_WORLD)
                 x[1:1,2:anzy+1]=obenbek
             else
-                MPI.Send(unten,convert(Int32,id+sqrt(size)),0,MPI.COMM_WORLD)
-                MPI.Send(oben,convert(Int32,id-sqrt(size)),0,MPI.COMM_WORLD)
-                MPI.Recv!(untenbek,convert(Int32,id+sqrt(size)),0,MPI.COMM_WORLD)
+                MPI.Send(unten,convert(Int64,id+sqrt(num_procs)),0,MPI.COMM_WORLD)
+                MPI.Send(oben,convert(Int64,id-sqrt(num_procs)),0,MPI.COMM_WORLD)
+                MPI.Recv!(untenbek,convert(Int64,id+sqrt(num_procs)),0,MPI.COMM_WORLD)
                 x[anzx+2:anzx+2,2:anzy+1]=untenbek
-                MPI.Recv!(obenbek,convert(Int32,id-sqrt(size)),0,MPI.COMM_WORLD)
+                MPI.Recv!(obenbek,convert(Int64,id-sqrt(num_procs)),0,MPI.COMM_WORLD)
                 x[1:1,2:anzy+1]=obenbek
             end
         else
             #in der letzten zeile wird nur nach oben getauscht
-            if floor(id/sqrt(size))==sqrt(size)-1
-                MPI.Recv!(obenbek,convert(Int32,id-sqrt(size)),0,MPI.COMM_WORLD)
+            if floor(id/sqrt(num_procs))==sqrt(num_procs)-1
+                MPI.Recv!(obenbek,convert(Int64,id-sqrt(num_procs)),0,MPI.COMM_WORLD)
                 x[1:1,2:anzy+1]=obenbek
-                MPI.Send(oben,convert(Int32,id-sqrt(size)),0,MPI.COMM_WORLD)
+                MPI.Send(oben,convert(Int64,id-sqrt(num_procs)),0,MPI.COMM_WORLD)
             else
-                MPI.Recv!(untenbek,convert(Int32,id+sqrt(size)),0,MPI.COMM_WORLD)
+                MPI.Recv!(untenbek,convert(Int64,id+sqrt(num_procs)),0,MPI.COMM_WORLD)
                 x[anzx+2:anzx+2,2:anzy+1]=untenbek
-                MPI.Recv!(obenbek,convert(Int32,id-sqrt(size)),0,MPI.COMM_WORLD)
+                MPI.Recv!(obenbek,convert(Int64,id-sqrt(num_procs)),0,MPI.COMM_WORLD)
                 x[1:1,2:anzy+1]=obenbek
-                MPI.Send(unten,convert(Int32,id+sqrt(size)),0,MPI.COMM_WORLD)
-                MPI.Send(oben,convert(Int32,id-sqrt(size)),0,MPI.COMM_WORLD)
+                MPI.Send(unten,convert(Int64,id+sqrt(num_procs)),0,MPI.COMM_WORLD)
+                MPI.Send(oben,convert(Int64,id-sqrt(num_procs)),0,MPI.COMM_WORLD)
             end
         end
     end 
@@ -131,7 +131,7 @@ function main(n::Int64, k::Int64)
    
     #es wird herausgefynden wie viele prozessore und welche id diese haben
     id = MPI.Comm_rank(MPI.COMM_WORLD)
-    size = MPI.Comm_size(MPI.COMM_WORLD)
+    num_procs = MPI.Comm_size(MPI.COMM_WORLD)
     
     globx=1
     globy=1
@@ -139,27 +139,27 @@ function main(n::Int64, k::Int64)
     anzy=0
     
     #die prozessorenanzahl darf nur eine quadratzahl sein
-    if size==0 && convert(Int32, sqrt(size))^2!=size 
-        if size!=1
-            println(size!=0 && convert(Int32, sqrt(size))^2==size)
+    if num_procs==0 && convert(Int64, sqrt(num_procs))^2!=num_procs 
+        if num_procs!=1
+            println(num_procs!=0 && convert(Int64, sqrt(num_procs))^2==num_procs)
             return 0
         end
     end
     
     #Die globalen x werte werden bestimmt und die matrixgroesse und das bedingt pro prozessor
-    globy=convert(Int64,ceil((n+1)/sqrt(size))*(id%sqrt(size))+1)
-    if id%sqrt(size)==sqrt(size)-1
+    globy=convert(Int64,ceil((n+1)/sqrt(num_procs))*(id%sqrt(num_procs))+1)
+    if id%sqrt(num_procs)==sqrt(num_procs)-1
         anzy=convert(Int64,n-globy+1)
     else
-        anzy=convert(Int64,ceil((n+1)/sqrt(size)))
+        anzy=convert(Int64,ceil((n+1)/sqrt(num_procs)))
     end
     
-    globx=convert(Int64,floor(id/sqrt(size))*ceil((n+1)/sqrt(size))+1)
+    globx=convert(Int64,floor(id/sqrt(num_procs))*ceil((n+1)/sqrt(num_procs))+1)
     
-    if id>=size-sqrt(size)
+    if id>=num_procs-sqrt(num_procs)
         anzx=convert(Int64,n-globx+1)
     else
-        anzx=convert(Int64, ceil((n+1)/sqrt(size)))
+        anzx=convert(Int64, ceil((n+1)/sqrt(num_procs)))
     end
     
     #Funktionen und A und x werden erzeugt
@@ -172,31 +172,32 @@ function main(n::Int64, k::Int64)
     x=fuege_Rand_hinzu(x)
     
     #Haloaustausch
-    halo_austausch!(id,size,x)
-    
+    halo_austausch!(id,num_procs,x, anzx, anzy)
     #Matrixmultiplikation
     x=matrixmultiplikation(A,x)
-    
     #Maximum bestimmen
     max=maximum(abs(fourier(f2,n,k,globx,globy,anzx,anzy)-x))
     y=MPI.Reduce(max, MPI.MAX, 0, MPI.COMM_WORLD)
-    if(id==0)
-        println("Error: ",y)
-    end
     return y
 end
 
 MPI.Init()
 id = MPI.Comm_rank(MPI.COMM_WORLD)
+num_procs = MPI.Comm_size(MPI.COMM_WORLD)
 n=7
 k=1
 if(id==0)
-   datei=open("Daten/matrixmultiplikation_zweidimensional_4_threads.txt","w")
+   str= "Daten/matrixmultiplikation_zweidimensional_$(num_procs).txt"
+   datei=open(str,"w")
 end
 while n<10000
-    error=main(n,k)
     if(id==0)
-        write(datei,string(n),";",string(k),";"string(error), ";\n")
+        t1=time()
+        error=main(n,k)
+        t2=time()-t1
+        write(datei,string(n),";",string(k),";"string(error),";",string(t2), ";\n")
+    else
+        error=main(n,k)
     end
     n=2*(n+1)-1
 end
---
+++ Visualisierung/Visualisierung_zeit_procs.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as pyplot
+import numpy as np
+
+python = open("/home/zam/heisters/projects/iterative_loeser/Parallelisierung/Daten/matrixmultiplikation_zweidimensional_1.txt", "r")
+python2 = open("/home/zam/heisters/projects/iterative_loeser/Parallelisierung/Daten/matrixmultiplikation_zweidimensional_4.txt", "r")
+python3 = open("/home/zam/heisters/projects/iterative_loeser/Parallelisierung/Daten/matrixmultiplikation_zweidimensional_9.txt", "r")
+python4 = open("/home/zam/heisters/projects/iterative_loeser/Parallelisierung/Daten/matrixmultiplikation_zweidimensional_16.txt", "r")
+
+n=[]
+zeit1=[]
+zeit4=[]
+zeit9=[]
+zeit16=[]
+
+for line in python:
+    array = line.split(";")
+    n.append(int(array[0]))
+    zeit1.append(float(array[3]))
+for line in python2:
+    array = line.split(";")
+    zeit4.append(float(array[3]))
+for line in python3:
+    array = line.split(";")
+    zeit9.append(float(array[3]))
+for line in python4:
+    array = line.split(";")
+    zeit16.append(float(array[3]))
+
+python.close()
+python2.close()
+python3.close()
+python4.close()
+
+pyplot.ylabel("Zeit")
+pyplot.xlabel("N")
+
+pyplot.semilogx(n,zeit1,"gv-", linewidth=1, markersize=7, label='1 Prozessor')
+pyplot.semilogx(n,zeit4,"ys-", linewidth=1, markersize=7, label='4 Prozessor')
+pyplot.semilogx(n,zeit9,"b+-", linewidth=1, markersize=7, label='9 Prozessor')
+pyplot.semilogx(n,zeit16,"rp-", linewidth=1, markersize=7, label='16 Prozessor')
+
+pyplot.legend(numpoints=1,loc=1)
+pyplot.grid()
+
+pyplot.tight_layout()
+pyplot.savefig('Visualisierung_fertig/visualisierung_matrixmultiplikation_zeit_procs.png', bbox_inches='tight')
